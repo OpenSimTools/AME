022eca3e4cf263360874d364f1af62fb5e462814
Paolo Ambrosio <git@paolo.ambrosio.name>
2023-10-25 09:04:27 +0200

    ReactiveUI boilerplate

--- src/GUI/ViewModels/MainWindowViewModel.cs
+++ src/GUI/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,75 @@
-﻿namespace AME.GUI.ViewModels;
+﻿using ReactiveUI;
+using System.Reactive;
+
+namespace AME.GUI.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string? Mass { get; set; }
-    public string? BodyDragBase { get; set; }
-    public string? GeneralTorqueMult { get; set; }
-    public string? GeneralPowerMult { get; set; }
-    public string? MaxForceAtSteeringRack { get; set; }
-    public string? PneumaticTrail { get; set; }
-    public string? PneumaticTrailGripFractPower { get; set; }
+    private string? mass;
+    public string? Mass
+    {
+        get => mass;
+        set => this.RaiseAndSetIfChanged(ref mass, value);
+    }
+
+    private string? bodyDragBase;
+    public string? BodyDragBase
+    {
+        get => bodyDragBase;
+        set => this.RaiseAndSetIfChanged(ref bodyDragBase, value);
+    }
+
+    private string? generalTorqueMult;
+    public string? GeneralTorqueMult
+    {
+        get => generalTorqueMult;
+        set => this.RaiseAndSetIfChanged(ref generalTorqueMult, value);
+    }
+
+    private string? generalPowerMult;
+    public string? GeneralPowerMult
+    {
+        get => generalPowerMult;
+        set => this.RaiseAndSetIfChanged(ref generalPowerMult, value);
+    }
+
+    private string? maxForceAtSteeringRack;
+    public string? MaxForceAtSteeringRack
+    {
+        get => maxForceAtSteeringRack;
+        set => this.RaiseAndSetIfChanged(ref maxForceAtSteeringRack, value);
+    }
+
+    private string? pneumaticTrail;
+    public string? PneumaticTrail
+    {
+        get => pneumaticTrail;
+        set => this.RaiseAndSetIfChanged(ref pneumaticTrail, value);
+    }
+
+    private string? pneumaticTrailGripFractPower;
+    public string? PneumaticTrailGripFractPower
+    {
+        get => pneumaticTrailGripFractPower;
+        set => this.RaiseAndSetIfChanged(ref pneumaticTrailGripFractPower, value);
+    }
+
+    public ReactiveCommand<Unit, Unit> OpenCommand { get; }
+    public ReactiveCommand<Unit, Unit> SaveCommand { get; }
+
+    public MainWindowViewModel()
+    {
+        OpenCommand = ReactiveCommand.Create(Open);
+        SaveCommand = ReactiveCommand.Create(Save);
+    }
+
+    public void Open()
+    {
+        System.Diagnostics.Debug.WriteLine($"Open Command");
+    }
+
+    public void Save()
+    {
+        System.Diagnostics.Debug.WriteLine($"Save Command");
+    }
 }
